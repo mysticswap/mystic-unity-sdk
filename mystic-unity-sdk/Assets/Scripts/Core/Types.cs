bca3bab5bc2564d3aebd224cf58128f4c89a05cf
ropok <jalerse@gmail.com>
2023-09-21 20:08:20 +0700

    Types update
    * SwapResponse
    * SwapData (signature, swapId)
    * SignatureRequest
    
    https://nftswap.atlassian.net/browse/DEV-816

--- mystic-unity-sdk/Assets/Scripts/Core/Types.cs
+++ mystic-unity-sdk/Assets/Scripts/Core/Types.cs
@@ -1,5 +1,7 @@
 ﻿#nullable enable
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace Core
 {
@@ -79,11 +81,113 @@ namespace Core
     }
 
     [System.Serializable]
+    public struct DomainData
+    {
+        public string name;
+        public string version;
+        public int chainId;
+        public string verifyingContract;
+    }
+
+    [System.Serializable]
+    public struct TypesComponents
+    {
+        public string name;
+        public string type;
+    }
+
+    [System.Serializable]
+    public struct ValueComponents
+    {
+        public int itemtype;
+        public string token;
+        public string identifierOrCriteria;
+        public string startAmount;
+        public string endAmount;
+        public string recipient;
+    }
+
+    [System.Serializable]
+    public struct Value
+    {
+        public string offerer;
+        public string zone;
+        public string zoneHash;
+        public string startTime;
+        public string endTime;
+        public int orderType;
+        public List<ValueComponents> offer;
+        public List<ValueComponents> consideration;
+        public int totalOriginalConsiderationItems;
+        public string salt;
+        public string conduitKey;
+        public int counter;
+    }
+    
+
+    [System.Serializable]
+    public struct ApprovalIsNeeded
+    {
+        public string data;
+        public string to;
+    }
+    
+    [System.Serializable]
+    public struct SignTypedMessage
+    {
+        public DomainData domainData;
+        public SignatureTypes types;
+        public Value value;
+    }
+
+    [System.Serializable]
+    public class SwapResponse
+    {
+        public SignTypedMessage signTypedMessage;
+        public List<ApprovalIsNeeded> approvalsNeeded;
+        public string swapId;
+    }
+
+    [System.Serializable]
     public struct SwapData
     {
         public string signature;
         public string swapId;
         public string takerAddress;
+    }
+
+    [System.Serializable]
+    public struct SignatureTypes
+    {
+        public List<TypesComponents> OrderComponents;
+        public List<TypesComponents> OfferItem;
+        public List<TypesComponents> ConsiderationItem;
+        public List<TypesComponents> EIP712Domain;
+    }
+
+    [System.Serializable]
+    public struct SignatureData
+    {
+        public DomainData domain;
+        public SignatureTypes types;
+        public Value message;
+        public string primaryType;
+        public string swapId;
+    }
+    
+    [System.Serializable]
+    public struct MetaMaskData
+    {
+        public string id;
+        public string jsonrpc;
+        public string result;
+    }
+
+    [System.Serializable]
+    public class MetaMaskResponse
+    {
+        public string name;
+        public MetaMaskData data;
     }
 
 }
