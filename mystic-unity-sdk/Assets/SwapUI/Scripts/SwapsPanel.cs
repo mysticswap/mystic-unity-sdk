7938b9f8d964642a97b66f6d116a709efdb0cb99
ropok <jalerse@gmail.com>
2023-10-11 21:24:26 +0700

    Load swaps, accept and cancel swaps
    * Scene: update swaps panel with load, accept, cancel function
    * SwapsPanel: script interaction for swaps interaction
    
    https://nftswap.atlassian.net/browse/DEV-818

--- mystic-unity-sdk/Assets/SwapUI/Scripts/SwapsPanel.cs
+++ mystic-unity-sdk/Assets/SwapUI/Scripts/SwapsPanel.cs
@@ -1,6 +1,7 @@
 ﻿using Core;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SwapsPanel : MonoBehaviour
 {
@@ -10,9 +11,15 @@ public class SwapsPanel : MonoBehaviour
     public List<NFT> NFTsOffer { get; private set; }
     public List<NFT> NFTsConsideration { get; private set; }
     public string SwapStatus { get; private set; }
+    private const string statusValidated = "validated";
+    private const string textButtonAccept = "Accept";
+    private const string textButtonCancel = "Cancel";
     [SerializeField] private NFTsItem nFTsItem;
     [SerializeField] private GameObject offerPanel;
     [SerializeField] private GameObject considerationPanel;
+    [SerializeField] private Button swapInteractionButton;
+    private bool isSwapAcceptable;
+    private SwapData swapData;
 
 
     public void Init(string creatorAddress, string takerAddress, string swapId, List<NFT> nFTsOffer, List<NFT> nFTsConsideration, string swapStatus)
@@ -25,6 +32,14 @@ public class SwapsPanel : MonoBehaviour
         SwapStatus = swapStatus;
         GenerateNFTsItem(NFTsOffer, nFTsItem, offerPanel);
         GenerateNFTsItem(NFTsConsideration, nFTsItem, considerationPanel);
+
+        isSwapAcceptable = IsSwapAcceptable(SwapStatus, CreatorAddress);
+        settingUpButton(isSwapAcceptable);
+        swapData = new SwapData()
+        {
+            swapId = SwapId,
+            takerAddress = TakerAddress,
+        };
     }
 
     public void GenerateNFTsItem(List<NFT> listNft, NFTsItem _nFTsItem, GameObject _parentPanel)
@@ -35,8 +50,50 @@ public class SwapsPanel : MonoBehaviour
             newNFTsItem.Init(nft.title, nft.rawMetadata.image);
 
             newNFTsItem.transform.SetParent(_parentPanel.transform);
-
         }
-
     }
+
+    private bool IsSwapAcceptable(string status, string address)
+    {
+        var connectedAddress = MysticSDKManager.Instance.sdk.GetAddress();
+
+        bool _isSwapAcceptable = (status == statusValidated && address != connectedAddress) ? true : false;
+        return _isSwapAcceptable;
+    }
+
+    private void settingUpButton(bool isAcceptButton)
+    {
+        string textButton;
+        if (isAcceptButton)
+        {
+            textButton = textButtonAccept;
+        }
+        else
+        {
+            textButton = textButtonCancel;
+        }
+        swapInteractionButton.GetComponentInChildren<Text>().text = textButton;
+    }
+
+    public async void OnClickSwapInteraction()
+    {
+        var sdk = MysticSDKManager.Instance.sdk;
+        if (isSwapAcceptable)
+        {
+            await sdk.AcceptSwap(swapData);
+        }
+        else
+        {
+            await sdk.CancelSwap(swapData);
+        }
+    }
+
+
+
+    //public void OnClickAcceptOrCancel()
+    //{
+
+
+
+    //}
 }
