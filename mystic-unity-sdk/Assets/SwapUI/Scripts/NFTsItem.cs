2dfb960783e7edc85251afc4c4c5526b7d031b24
ropok <jalerse@gmail.com>
2023-10-11 09:03:32 +0700

    Load My Swaps in-game
    * NFTsItem: as a single item in prefab with script
    * Panel_Swaps: as a single swap in prefab with script
    * Scene: setting up load swaps
    
    https://nftswap.atlassian.net/browse/DEV-837

---
+++ mystic-unity-sdk/Assets/SwapUI/Scripts/NFTsItem.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class NFTsItem : MonoBehaviour
+{
+    public string Title { get; private set; }
+    public string UrlImage { get; private set; }
+    [SerializeField] private TextMeshProUGUI titleText;
+
+    public void Init(string title, string urlImage)
+    {
+        Title = title;
+        UrlImage = urlImage;
+        titleText.text = Title;
+    }
+}
---
+++ mystic-unity-sdk/Assets/SwapUI/Scripts/SwapsPanel.cs
@@ -0,0 +1,42 @@
+﻿using Core;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapsPanel : MonoBehaviour
+{
+    public string CreatorAddress { get; private set; }
+    public string TakerAddress { get; private set; }
+    public string SwapId { get; private set; }
+    public List<NFT> NFTsOffer { get; private set; }
+    public List<NFT> NFTsConsideration { get; private set; }
+    public string SwapStatus { get; private set; }
+    [SerializeField] private NFTsItem nFTsItem;
+    [SerializeField] private GameObject offerPanel;
+    [SerializeField] private GameObject considerationPanel;
+
+
+    public void Init(string creatorAddress, string takerAddress, string swapId, List<NFT> nFTsOffer, List<NFT> nFTsConsideration, string swapStatus)
+    {
+        CreatorAddress = creatorAddress;
+        TakerAddress = takerAddress;
+        SwapId = swapId;
+        NFTsOffer = nFTsOffer;
+        NFTsConsideration = nFTsConsideration;
+        SwapStatus = swapStatus;
+        GenerateNFTsItem(NFTsOffer, nFTsItem, offerPanel);
+        GenerateNFTsItem(NFTsConsideration, nFTsItem, considerationPanel);
+    }
+
+    public void GenerateNFTsItem(List<NFT> listNft, NFTsItem _nFTsItem, GameObject _parentPanel)
+    {
+        foreach (var nft in listNft)
+        {
+            var newNFTsItem = Instantiate(_nFTsItem, transform.position, transform.rotation);
+            newNFTsItem.Init(nft.title, nft.rawMetadata.image);
+
+            newNFTsItem.transform.SetParent(_parentPanel.transform);
+
+        }
+
+    }
+}
